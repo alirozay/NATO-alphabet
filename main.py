e24b580c9aeb0c9a6af736c3f068694727233fb2
alirozay <44883412+alirozay@users.noreply.github.com>
2022-05-14 22:49:17 -0400

    Added error catching functionality

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pandas
+is_right = False
 nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index, row) in nato_data_frame.iterrows()}
-user_input = input("Type in your name: ").upper()
-user_nato = [nato_dict[letter] for letter in user_input]
+while not is_right:
+    user_input = input("Type in your name: ").upper()
+    try:
+        user_nato = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only alphabets are allowed")
+    else:
+        is_right = True
 print(user_nato)
